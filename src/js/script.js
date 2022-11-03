a12a584e71ab694ed14049032f5122f15031f949
Benjamin <benjamin.lameko@gmail.com>
2022-11-03 17:27:44 +0300

    Add sending emails to the server & page up scroll

--- src/js/script.js
+++ src/js/script.js
@@ -82,7 +82,58 @@ $(document).ready(function(){
 	valideForms('#consultation form');
 	valideForms('#order form');
 
-	$('input[name=phone]').mask("+7 (999) 999-99-99");
+	$('input[name=phone]').mask("+375 (99) 999-99-99");
+
+	$('form').submit(function(e) {
+		e.preventDefault();
+
+		if(!$(this).valid()) {
+			return;
+		}
+
+		$.ajax ({
+			type:"POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			$('#consultation, #order').fadeOut();
+			$('.overlay, #thanks').fadeIn('slow');
+
+			$('form').trigger('reset');
+		});
+		return false;
+	});
+
+	// Smooth scroll and pageup
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 1600) {
+			$('.pageup').fadeIn();
+		} else {
+			$('.pageup').fadeOut();
+		}
+	});
+
+	// Add smooth scrolling to all links
+	
+	$("a").on('click', function(event) {
+		// Make sure this.hash has a value before overriding default behavior
+		if (this.hash !== "") {
+		  // Prevent default anchor click behavior
+			event.preventDefault();
+		  // Store hash
+			var hash = this.hash;
+		  // Using jQuery's animate() method to add smooth page scroll
+		  // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+		  	}, 300, function(){
+			// Add hash (#) to URL when done scrolling (default click behavior)
+			window.location.hash = hash;
+		  });
+		} // End if
+	});
 });
 
 
