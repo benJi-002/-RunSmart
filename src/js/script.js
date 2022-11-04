617d74340b5ba7a1989d1d5551400e686222705c
Benjamin <benjamin.lameko@gmail.com>
2022-11-04 18:59:28 +0300

    Validation

--- src/js/script.js
+++ src/js/script.js
@@ -22,6 +22,8 @@ $(document).ready(function(){
 	.closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
   });
 
+  
+
   function toggleSlide(item) {
   $(item).each(function(i) {
 	$(this).on('click', function(e) {
